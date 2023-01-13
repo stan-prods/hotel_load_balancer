5c277d80cb1ff454c3a1ea59ea6b4826c4d2c80a
Stan Alexson <prods.by.stan@gmail.com>
2023-01-13 19:16:23 +0200

    autostart initial

---
+++ autostart/autostart.ino
@@ -0,0 +1,33 @@
+/*
+    Outputs:
+    1 - status led(s)
+    2 - servo
+    3 - main contactor control voltage
+
+        Relays:
+        1 - power source select generator/line
+        2 - gas valve
+        3 - gas valve powering from battery (for egnition)
+        4 - egnition key position
+
+    Inputs:
+    1 - 5v when line is present
+    2 - 5v when generator is started working
+    3 - manual gas valve control switch
+    4 - manual servo control pot
+
+
+    powering from usb
+    if generator is powerking, then switch to charge from gen. Else charge from mains
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
