0754d11ab9577030c6d39685d9010ada46524065
Stan Alexson <prods.by.stan@gmail.com>
2023-01-15 23:15:42 +0200

    first working

--- autostart/autostart.ino
+++ autostart/autostart.ino
@@ -22,41 +22,93 @@
 */
 
 //inputs
-#define mainsPresentCV 4
-#define generatorBasedPowerPin 5
+#define generatorBasedPowerPin 34
+#define mainsPresentCV 35
 
 //outputs
-#define powerSourceSelectRelayPin 6
-#define gasValveRelayPin 7
-#define gasValvePowerSourceSelectPin 8
-#define starterRelayPin 9
+#define powerSourceSelectRelayPin 27
+#define gasValveRelayPin 26
+#define gasValvePowerSourceSelectPin 25
+#define starterRelayPin 33
 
 //constants
 #define gasValveOpenDelaySecs 5
 #define starterActiveDelaySecs 10
+#define starterPauseDelaySecs 20
 #define generatorCooldownDelaySecs 10
+#define allowedAttempts 4
 
+enum Activity {enable, disable};
 
-
-bool isStarterRunning, isGeneratorWorking, isMainsPresent;
+bool isStarterRunning, isGeneratorWorking, isMainsPresent, isValveOpen, isValveOnBattery;
 uint8_t attempts;
 
 unsigned long secs;
+unsigned long msecs;
 unsigned long starterEnabledTimestampSecs;
 unsigned long generatorCooldownStartedTimestampSecs;
+unsigned long starterPauseStartedTimestampSecs;
+unsigned long inputValueChangedTimestampMsecs;
+unsigned long gasValveOpenOnStartTimestampSecs;
 
 
 void setup() {
+    Serial.begin(115200);
+    pinMode(mainsPresentCV, INPUT);
+    pinMode(generatorBasedPowerPin, INPUT);
+
+    pinMode(powerSourceSelectRelayPin, OUTPUT);
+    digitalWrite(powerSourceSelectRelayPin, disable);
+
+    pinMode(gasValveRelayPin, OUTPUT);
+    digitalWrite(gasValveRelayPin, disable);
+
+    pinMode(gasValvePowerSourceSelectPin, OUTPUT);
+    digitalWrite(gasValvePowerSourceSelectPin, disable);
+
+    pinMode(starterRelayPin, OUTPUT);
+    digitalWrite(starterRelayPin, disable);
 
 }
 
 void checkStatus() {
-    isGeneratorWorking = digitalRead(generatorBasedPowerPin);
-    isMainsPresent = digitalRead(mainsPresentCV);
+    //TODO add debounce
+    bool genPowerVal = digitalRead(generatorBasedPowerPin);
+    bool mainsPowerVal = digitalRead(mainsPresentCV);
+
+    if (genPowerVal != isGeneratorWorking || mainsPowerVal != isMainsPresent) {
+        if (!inputValueChangedTimestampMsecs) {
+            inputValueChangedTimestampMsecs = msecs;
+        }
+    }
+
+    if (inputValueChangedTimestampMsecs > 0 && msecs - inputValueChangedTimestampMsecs > 15) {
+        attempts = 0;
+        inputValueChangedTimestampMsecs = 0;
+        isGeneratorWorking = genPowerVal;
+        isMainsPresent = mainsPowerVal;
+    }
+
+    Serial.println("");
+    Serial.println("");
+    Serial.print(" Generator:");
+    Serial.print(isGeneratorWorking);
+    Serial.print(" Mains:");
+    Serial.print(isMainsPresent);
+    Serial.print(" Valve:");
+    Serial.print(isValveOpen);
+    Serial.print(" ValvePS:");
+    Serial.print(isValveOnBattery);
+    Serial.print(" Starter:");
+    Serial.print(isStarterRunning);
+    Serial.print(" Attempts:");
+    Serial.print(attempts);
+    Serial.println("");
 }
 
 void loop() {
-    secs = millis() / 1000;
+    msecs = millis();
+    secs = msecs / 1000;
 
     checkStatus();
     checkStarter();
@@ -69,52 +121,83 @@ void loop() {
         stopGenerator();
     }
 
+    if (!isGeneratorWorking && isMainsPresent) {
+        digitalWrite(gasValvePowerSourceSelectPin, disable);
+        isValveOnBattery = false;
+        digitalWrite(gasValveRelayPin, disable);
+        isValveOpen = false;
+        stopStarter();
+    }
 
+    if (isGeneratorWorking && isValveOnBattery) {
+        digitalWrite(gasValvePowerSourceSelectPin, disable);
+        isValveOnBattery = false;
+    }
+
+    powerSelect();
+
+    delay(30);
 }
 
 void powerSelect() {
-    digitalWrite(powerSourceSelectRelayPin, isGeneratorWorking)
+    digitalWrite(powerSourceSelectRelayPin, !isGeneratorWorking || isMainsPresent);
 }
 
 
 void startGenerator() {
-    if (attempts >= 3) {
+    Serial.print(", Start generator");
+    if (attempts > allowedAttempts - 1) {
         return;
     }
     //open gas valve
-    digitalWrite(gasValvePowerSourceSelectPin, HIGH);
-    digitalWrite(gasValveRelayPin, HIGH);
+    digitalWrite(gasValvePowerSourceSelectPin, enable);
+    isValveOnBattery = true;
+
+    digitalWrite(gasValveRelayPin, enable);
+    isValveOpen = true;
 
     //if first attempt, let gas flow
-    if (attempts == 0) {
-        delay(gasValveOpenDelaySecs * 1000);
+    if (attempts == 0 && gasValveOpenOnStartTimestampSecs == 0) {
+        gasValveOpenOnStartTimestampSecs = secs;
     }
 
-    runStarter();
+    if (secs - gasValveOpenOnStartTimestampSecs > gasValveOpenDelaySecs) {
+        gasValveOpenOnStartTimestampSecs = 0;
+        runStarter();
+    }
+
 }
 
 void stopGenerator() {
+    Serial.print(", Stop generator");
     if (!generatorCooldownStartedTimestampSecs) {
-        generatorCooldownDelaySecs = secs;
+        generatorCooldownStartedTimestampSecs = secs;
     }
 
     if (secs - generatorCooldownStartedTimestampSecs >= generatorCooldownDelaySecs) {
-        digitalWrite(gasValveRelayPin, LOW);
+        digitalWrite(gasValveRelayPin, disable);
+        isValveOpen = false;
         generatorCooldownStartedTimestampSecs = 0;
     }
 }
 
 void runStarter() {
-    if (!isStarterRunning) {
-        digitalWrite(starterRelayPin, HIGH);
+    if (isGeneratorWorking) {
+        return;
+    }
+    Serial.print(", Run Starter");
+    if (!isStarterRunning && secs - starterPauseStartedTimestampSecs > starterPauseDelaySecs) {
+        digitalWrite(starterRelayPin, enable);
         starterEnabledTimestampSecs = secs;
         isStarterRunning = true;
     }
 }
 
 void stopStarter() {
-    digitalWrite(starterRelayPin, LOW);
+    Serial.print(", Stop Starter");
+    digitalWrite(starterRelayPin, disable);
     starterEnabledTimestampSecs = 0;
+    gasValveOpenOnStartTimestampSecs = 0;
     isStarterRunning = false;
 }
 
@@ -123,18 +206,31 @@ void checkStarter() {
         return;
     }
 
+    if (isMainsPresent) {
+        stopStarter();
+        attempts = 0;
+        digitalWrite(gasValvePowerSourceSelectPin, disable);
+        isValveOnBattery = false;
+        digitalWrite(gasValveRelayPin, disable);
+        isValveOpen = false;
+    }
+
     if (isGeneratorWorking) {
+        Serial.print(", Generator started");
         stopStarter();
         attempts = 0;
 
-        digitalWrite(gasValvePowerSourceSelectPin, LOW);
-    }
-
-    if (secs - starterEnabledTimestampSecs >= starterActiveDelaySecs) {
+        digitalWrite(gasValvePowerSourceSelectPin, disable);
+        isValveOnBattery = false;
+    } else if (secs - starterEnabledTimestampSecs >= starterActiveDelaySecs) {
+        Serial.print(", Generator start failed");
         stopStarter();
         attempts++;
 
-        digitalWrite(gasValvePowerSourceSelectPin, LOW);
-        digitalWrite(gasValveRelayPin, LOW);
+        digitalWrite(gasValvePowerSourceSelectPin, disable);
+        isValveOnBattery = false;
+        digitalWrite(gasValveRelayPin, disable);
+        isValveOpen = false;
+        starterPauseStartedTimestampSecs = secs;
     }
 }
